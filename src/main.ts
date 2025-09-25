bf9f8742be7178e2e467c9443507881aace7ed1d
Andre Giske <AndreGiske@MacBook-Air.local>
2025-09-25 12:27:20 -0700

    lemonade stand

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+class Lemonade_Stand {
+	make_lemonade(){
+		// remove ingredients from inventory
+	}
+
+	count_inventory(){
+		// return inventory status
+	}
+}
+
+interface inventory {
+	lemons: number;
+	sugar: number;
+	cups: number;
+}
