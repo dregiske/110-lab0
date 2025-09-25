0bea10324b0cf562ab5200cc0b2ede50252aa64a
Andre Giske <AndreGiske@MacBook-Air.local>
2025-09-25 12:40:56 -0700

    fucntions

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
-import { CustomPromisify } from "util";
+// Our implementation of a lemonade stand
+// Andre Giske & Luis Herrera
 
 class Lemonade_Stand {
 	inventory: Inventory;
+	balance: number;
+	day: number;
+	customers: number;
 
+	constructor() {
+		this.inventory = new Inventory();
+		this.balance = 0;
+		this.day = 1;
+		this.customers = 0;
+	}
 
+	validate_sale(){
+		// make sure we have ingredients before sale
+	}
 
 	make_lemonade(){
-		// remove ingredients from inventory
+		// subtract from inventory
+		// increase day count
+		// increase balance
 	}
 
 	count_inventory(){
@@ -19,4 +34,11 @@ class Inventory {
 	ice: number;
 	sugar: number
 	lemons: number;
+
+	constructor(){
+		this.cups = 10;
+		this.ice = 15;
+		this.sugar = 10;
+		this.lemons = 10;
+	}
 }
